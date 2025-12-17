0ea4da45f1ac01d3eacc997cfb0754f58b15496c
hipster-amz <amz@hipster.dev>
2025-12-17 13:50:13 -0600

    Add minimal amplify_outputs.json and restore Amplify import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Amplify } from 'aws-amplify'
+import outputs from '../../amplify_outputs.json'
 import App from './App'
+
+Amplify.configure(outputs)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
